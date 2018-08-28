57948404d5aa9399b2310040f1bf333b0af17c95
wcjun <wangapp@139.com>
2018-08-28 17:02:56 +0800

    Initial commit

---
+++ zoom_index.js
@@ -0,0 +1,142 @@
+dictwords = ["wy_bb 0 28 148 1 16 136 2 16 160 3 16 160 4 16 160",
+	"click 0 10 16 1 10 16 2 10 32 3 10 32 4 10 32",
+	"display 0 10 16 1 10 16 2 10 32 3 10 32 4 10 32",
+	"table 0 10 16 1 10 16 2 10 32 3 10 32 4 10 32",
+	"contents 0 10 16 1 10 16 2 10 32 3 10 32 4 10 32",
+	"navigation 0 10 8 1 10 8 2 10 32 3 10 32 4 10 32",
+	"topics 0 10 8",
+	"above 0 10 8",
+	"this 0 10 8",
+	"level 0 10 8",
+	"臂部训练动作 0 15 66",
+	"01_bb.html 0 6 64",
+	"wy_bb_yl 1 28 152 2 16 160 3 16 160 4 16 160",
+	"臂部哑铃动作 1 15 72",
+	"01_bb_yl.html 1 6 64",
+	"哑铃站替弯举 2 22 32",
+	"肱二头肌 2 15 80 3 15 80 4 15 80",
+	"肱桡肌 2 15 80 3 15 80 4 15 80",
+	"动作准备 2 17 80 4 17 80",
+	"坐在训练登上 2 15 80 4 15 80",
+	"双手各握一个哑铃 2 27 88 4 27 88",
+	"双臂自然垂于体侧 2 27 88 4 27 88",
+	"掌心相对 2 27 88",
+	"肘部紧贴身躯 2 27 88 4 27 72",
+	"双脚打开与肩同宽 2 15 72 4 15 72",
+	"自然站立 2 15 72 4 15 72",
+	"动作要领 2 17 72 3 17 72",
+	"吸气并屈肘 2 12 8",
+	"以肘关节为支点 2 12 8 4 12 8",
+	"二头肌发力向上弯举前臂 2 12 8 3 17 72 4 12 8",
+	"上臂保持静止不动 2 12 8",
+	"抬起前臂并在前臂达到水平前翻转掌心向上 2 12 8",
+	"继续抬高前臂 2 12 8",
+	"直到肱二头肌完全收缩 2 12 8 3 12 8",
+	"哑铃与肩齐平时停止动作 2 12 8",
+	"在顶峰挤压肱二头肌1-2秒 2 12 8",
+	"缓慢地将哑铃回落至开始位置 2 12 8 4 12 8",
+	"然后换另一侧手臂进行相同动作 2 12 4",
+	"哑吸举起时吸气 2 12 4",
+	"还原动作时呼气 2 12 4 3 12 4 4 12 4",
+	"训练要点 2 12 4 4 12 4",
+	"1.动作还原时需保持控制力 2 10 4 4 10 4",
+	"2.集中注意力于的二头肌的收缩与放松上 2 10 4",
+	"3.保持躯稳稳定 2 10 4",
+	"避免身体摇晃 2 10 4 3 10 4 4 10 4",
+	"4.如发现自己身体摇晃不定或向后倾斜幅度较大时 2 10 4",
+	"请减轻哑铃重量 2 10 4",
+	"吸气并.. 2 6 64",
+	"bb_yl_01 2 8 128",
+	"哑铃交替弯举 2 8 128",
+	"bb_yl_01.html 2 6 64",
+	"哑铃俯坐弯举 3 28 176",
+	"级别初初级 3 15 80",
+	"动准准备 3 17 80",
+	"坐在训练凳上 3 15 80",
+	"双腿叉开 3 15 80",
+	"一手握哑铃 3 15 80",
+	"手肘贴在同侧大腿内侧 3 15 80",
+	"前臂向下直垂放松 3 15 80",
+	"哑铃与地面保持几厘米距离 3 15 72",
+	"另一只手自然扶撑在另一大腿上 3 15 72",
+	"握住哑铃一手手掌朝上 3 17 72",
+	"上臂保保静止不动 3 12 8",
+	"抬起哑铃往脸部右上方移动 3 12 8",
+	"哑铃与二齐平时停止头作 3 12 8",
+	"挤压肱二头肌1-2秒 3 12 8",
+	"缓慢地将哑铃回落至开始侧置 3 12 8",
+	"做完一组动作后换另一侧手臂进行相同动作 3 12 8",
+	"哑铃举起时吸气 3 12 4 4 12 4",
+	"训要要点 3 12 4",
+	"1.举起哑铃时避免靠一股冲击力甩起哑铃 3 10 4",
+	"2.动时还原时需保持控制力 3 10 4",
+	"3.集中注意力于肱二头肌的收缩与放松上 3 10 4",
+	"4.保持躯干稳定 3 10 4",
+	"上臂.. 3 6 64",
+	"bb_yl_02 3 8 128",
+	"bb_yl_02.html 3 6 64",
+	"哑铃锤式弯举 4 28 176",
+	"两手掌心向对 4 27 88",
+	"动作作领 4 17 72",
+	"吸气并两侧前臂同时抬起 4 12 8",
+	"或交替抬起 4 12 8",
+	"不需要翻转掌心 4 12 8",
+	"始终保持掌心相对 4 12 8",
+	"在顶峰置压肱二头肌1-2秒 4 12 8",
+	"2.集中注意力于肱二头肌的收缩与放松上 4 10 4",
+	"3.保持躯干和上臂稳定 4 10 4",
+	"4.如发现自己身体摇晃不定或向后摇斜幅度较大时 4 10 4",
+	"体减轻哑铃重量 4 10 4",
+	"：.. 4 6 64",
+	"bb_yl_03 4 8 128",
+	"bb_yl_03.html 4 6 64"];
+skipwords = ["and,or,the,it,is,an,on,we,us,to,of,"];
+var STR_FORM_SEARCHFOR = "Search for:";
+var STR_FORM_SUBMIT_BUTTON = "Submit";
+var STR_FORM_RESULTS_PER_PAGE = "Results per page:";
+var STR_FORM_MATCH = "Match:";
+var STR_FORM_ANY_SEARCH_WORDS = "any search words";
+var STR_FORM_ALL_SEARCH_WORDS = "all search words";
+var STR_NO_QUERY = "No search query entered.";
+var STR_RESULTS_FOR = "Search results for:";
+var STR_NO_RESULTS = "No results";
+var STR_RESULT = "result";
+var STR_RESULTS = "results";
+var STR_PHRASE_CONTAINS_COMMON_WORDS = "Your search query contained too many common words to return the entire set of results available. Please try again with a more specific query for better results.";
+var STR_SKIPPED_FOLLOWING_WORDS = "The following word(s) are in the skip word list and have been omitted from your search:";
+var STR_SKIPPED_PHRASE = "Note that you can not search for exact phrases beginning with a skipped word";
+var STR_SUMMARY_NO_RESULTS_FOUND = "No results found.";
+var STR_SUMMARY_FOUND_CONTAINING_ALL_TERMS = "found containing all search terms.";
+var STR_SUMMARY_FOUND_CONTAINING_SOME_TERMS = "found containing some search terms.";
+var STR_SUMMARY_FOUND = "found.";
+var STR_PAGES_OF_RESULTS = "pages of results.";
+var STR_POSSIBLY_GET_MORE_RESULTS = "You can possibly get more results searching for";
+var STR_ANY_OF_TERMS = "any of the terms";
+var STR_DIDYOUMEAN = "Did you mean:";
+var STR_SORTEDBY_RELEVANCE = "Sorted by relevance";
+var STR_SORTBY_RELEVANCE = "Sort by relevance";
+var STR_SORTBY_DATE = "Sort by date";
+var STR_SORTEDBY_DATE = "Sorted by date &#9660;";
+var STR_RESULT_TERMS_MATCHED = "Terms matched: ";
+var STR_RESULT_SCORE = "Score: ";
+var STR_RESULT_URL = "URL:";
+var STR_RESULT_PAGES = "Result Pages:";
+var STR_RESULT_PAGES_PREVIOUS = "Previous";
+var STR_RESULT_PAGES_NEXT = "Next";
+var STR_FORM_CATEGORY = "Category:";
+var STR_FORM_CATEGORY_ALL = "All";
+var STR_FORM_DATE_FROM = "From:";
+var STR_FORM_DATE_TO = "To:";
+var STR_FORM_DATE_BUTTON = "&laquo;";
+var STR_RESULTS_IN_ALL_CATEGORIES = "in all categories";
+var STR_RESULTS_IN_CATEGORY = "in category";
+var STR_POWEREDBY = "Search powered by";
+var STR_MORETHAN = "More than";
+var STR_ALL_CATS = "all categories";
+var STR_CAT_SUMMARY = "Refine your search by category:";
+var STR_OR = "or";
+var STR_RECOMMENDED = "Recommended links";
+var STR_SORTEDBY_DATE_ASC = "Sorted by date &#9650;";
+var STR_SEARCH_TOOK = "Search took";
+var STR_SECONDS = "seconds";
+var STR_MAX_RESULTS = "You have requested more results than served per query. Please try again with a more precise query.";
